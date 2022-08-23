9210e417c8dd39fa2687824b163a2f65277b00f8
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-08-23 09:40:28 +0600

    Player Select

--- calculate.js
+++ calculate.js
@@ -1,4 +1,29 @@
-// Get the player by a onclick="function getElementByName(this)" function.
-function getElementByName(name) {
-    console.log(name.parentNode.parentNode.children[0].innerText);
-}
+// 
+
+document.getElementById('player-container').addEventListener("click", function (event) {
+
+    if (event.target.innerText == 'Select') {
+        const playerName = event.target.parentNode.parentNode.children[0].innerText;
+
+        const playerCount = document.querySelectorAll("#players li");
+        // for (let i = 0; i <playerCount.length ; i++)
+        const playerCountLength = playerCount.length;
+        console.log(playerCountLength);
+        if (playerCountLength > 4) {
+            return alert("You can't select more than five!");
+        }
+    
+        const playerList = document.getElementById('players');
+        const li = document.createElement("li");
+      
+        li.append(playerName);
+        playerList.append(li);
+       event.target.setAttribute("disabled", "true");
+       
+    }   
+    });
+       
+    
+
+
+    

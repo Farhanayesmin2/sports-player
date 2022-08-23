6b8fa9336ed2f071c84bb6147e2d3128b89921ad
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-08-23 17:17:05 +0600

    Comment Added

--- budget.js
+++ budget.js
@@ -9,17 +9,18 @@ function nameEveryPlayer() {
     const playerCount = perPlayerList.length;
    // multiply by each player cost
     const totalPayer = playerCount * perPlayer;
-   
+   // clear the input field 
     perPlayerValue.value = "";
     return totalPayer;
 }
-
+// Create a function for Number type input field.
  function getTheValue(number) {
     const getValueNumber = document.getElementById(number);
     const getValueNumberString = getValueNumber.value;
     const getTheNumber = parseInt(getValueNumberString);
     return getTheNumber;
 } 
+// Create a function for innerText Value.
 function getTheInnerTextValue(innerValue) {
     const getValueInnerText = document.getElementById(innerValue);
     const getValueInnerTextString = getValueInnerText.innerText;
@@ -28,7 +29,7 @@ function getTheInnerTextValue(innerValue) {
 } 
 
 
-
+// Create a function for set the innerText
 function setTheValueInnerText(number, newValue) {
     const setValueNumber = document.getElementById(number);
     const setValueNumberString = setValueNumber.innerText;
@@ -40,35 +41,54 @@ function setTheValueInnerText(number, newValue) {
 // calculate button for player cost
 document.getElementById('calculate-btn').addEventListener('click', function () {
 
-    // called the function in calculate button
+    // called the  nameEveryPlayer() function in calculate button.
    const player= nameEveryPlayer();
-  
+  // called the  getTheInnerTextValue() function.
     const playerExpenses = getTheInnerTextValue('player-expenses'); 
-   
+   // here, set the innerText value setTheValueInnerText(number, newValue).
     const playerExpensesTotal = setTheValueInnerText('player-expenses', player);
-// 
- /*    const manager = setTheValueInnerText('manager-field');
-
-    const coach = setTheValueInnerText('coach-field');
-    const theTotalCost = playerExpensesTotal + manager + coach;
-    console.log(theTotalCost); */
+// Called id="per-player" for alert if input field in not a valid number.
+    const alertTextPerPlayerInput = document.getElementById('per-player');
+if(isNaN( alertTextPerPlayerInput) )  {
+    return alert("Please Give The Correct Amount.");
+    }
 })
 
+// Create a function for event Handler for Total calculate Button.
 document.getElementById('total-calculate-btn').addEventListener('click', function () {
 
-  // get the all player cost
+  // Get the all player Cost value for addition for total amount
   const playerExpensesTotalAmount =getTheInnerTextValue('player-expenses'); 
     
+    // called the getTheValue() function for manager input field and coach input field value for addition for total amount.
     const manager = getTheValue('manager-field');
    
     const coach = getTheValue('coach-field');
    
     const addTheTwoValue = manager + coach;
-
+// Addition the total amount.
     const theTotalCost = playerExpensesTotalAmount + addTheTwoValue;
-   
+   // called the  getTheInnerTextValue(); for get number value.
     getTheInnerTextValue('total-cost');
+    // set the number value that's whay called setTheValueInnerText(number, newValue) . 
     setTheValueInnerText('total-cost', theTotalCost)
-   // console.log(theTotalCost);
-    
+   // Shpw the alert text for 
+   /* if (typeof manager != 'number' ) {
+    return alert("Can't select more string");
+   } */
+    const managerInput = document.getElementById('manager-field');
+    const coachInput = document.getElementById('coach-field');
+
+    // show the alert text when give wrong number
+ if(isNaN( managerInput) ||  isNaN(coachInput) )  {
+    return alert("Please Give The Correct Amount.");
+    }
 })
+
+
+/* if (typeof manager != 'number' ) {
+    return alert("Can't select more string");
+}
+else if(manager <0) {
+    return alert("Can't select more string");
+} */
--- calculate.js
+++ calculate.js
@@ -1,25 +1,30 @@
-// 
+// Take a event handler
 
 document.getElementById('player-container').addEventListener("click", function (event) {
 
     if (event.target.innerText == 'Select') {
         const playerName = event.target.parentNode.parentNode.children[0].innerText;
+        // Here,playerName varible to Get 'Select' text from button click.
 
         const playerCount = document.querySelectorAll("#players li");
-     
-        const playerCountLength = playerCount.length+1;
+     // querySelectorAll for get the all list item from id="players".
+        
+        //  Count the list item Length.
+        const playerCountLength = playerCount.length + 1;
+        
         const empty = playerCountLength + ".  ";
         const name = empty + playerName;
-        console.log(name);
+        // Condition add for alert text for more then 5 player added.
         if (playerCountLength > 5) {
             return alert("Can't select more than five Player.");
         }
-    
+    // Take a players list from id="players".
         const playerList = document.getElementById('players');
         const li = document.createElement("li");
-      
+      // Here, Append the list items.
         li.append(name);
         playerList.append(li);
+        // Here,After select Disable the button.
         const disabled = event.target.setAttribute("disabled", "true");
         // Change the color of disable button
      if (disabled != false) {
